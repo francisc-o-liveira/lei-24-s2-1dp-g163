ba44b642e2ef1776898352249e83efeb36caaa73
Francisco <francisc.o.liveira@outlook.com>
2024-04-15 20:28:22 +0100

    TEMPLATE READY TO IMPLEMENT CODE (ORGANIZATION REPO SOLVE)

--- src/test/java/pt/ipp/isep/dei/esoft/project/repository/OrganizationRepositoryTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/repository/OrganizationRepositoryTest.java
@@ -1,13 +1,5 @@
 package pt.ipp.isep.dei.esoft.project.repository;
 
-import org.junit.jupiter.api.Test;
-import pt.ipp.isep.dei.esoft.project.domain.Employee;
-import pt.ipp.isep.dei.esoft.project.domain.Organization;
-
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class OrganizationRepositoryTest {
 
 }
