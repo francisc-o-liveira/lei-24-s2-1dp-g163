e3a952d3a40d15ac69a8d21e35760849d8cd299c
Francisco <francisc.o.liveira@outlook.com>
2024-04-15 10:54:15 +0100

    Code Template

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/HRManagerUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/HRManagerUI.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HRManagerUI implements Runnable{
-    public HRManager() {
+    public HRManagerUI() {
     }
 
     public void run() {
