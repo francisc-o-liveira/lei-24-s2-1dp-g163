a7fc8e5971a01b482a4cf137aa506831c532ea62
susanaml27 <1220804@isep.ipp.pt>
2024-04-16 21:20:38 +0100

    start GenerateTeamController

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/TeamRepository.java
@@ -0,0 +1,37 @@
+package pt.ipp.isep.dei.esoft.project.repository;
+
+import pt.ipp.isep.dei.esoft.project.domain.Skill;
+import pt.ipp.isep.dei.esoft.project.domain.Team;
+
+import java.util.List;
+
+public class TeamRepository {
+
+    public List<Team> getTeam(){
+
+    }
+
+    public List<Team> getTeamBySkills(Skill skills){
+
+    }
+
+    public List<Team> getTeam(){
+
+    }
+
+    public List<Team> getTeamBySkills(Skill skill){
+        if(team.skill==skill){
+
+        }
+    }
+
+    public List<Skill> getSkillList(){
+
+    }
+
+
+
+
+
+
+}
