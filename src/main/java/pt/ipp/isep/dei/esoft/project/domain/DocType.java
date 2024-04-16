71b8c1bb909ea91676b45debafbd47fddf489eab
Francisco <francisc.o.liveira@outlook.com>
2024-04-16 17:38:21 +0100

    Updates in all camps

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/DocType.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/DocType.java
@@ -1,10 +1,35 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
 public class DocType {
-    private enum Type {CitizenCard, BilheteIdentidade};
+    public enum Type {CitizenCard, BilheteIdentidade, Passport};
     private Type docType;
 
     public DocType(Type docType){
         this.docType=docType;
     }
+
+    public boolean verifyDocType(int numberID){
+        boolean valueVerify = false;
+        switch (this.docType) {
+            case CitizenCard:
+                if(numberID){
+                    valueVerify=true;
+                }
+            case BilheteIdentidade:
+                if(numberID){
+                    valueVerify=true;
+                }
+            case Passport:
+                if(numberID){
+                    valueVerify=true;
+                }
+        }
+        return valueVerify;
+    }
+
+    public Type[] getDocTypesValues() {
+        return Type.values();
+    }
 }
+
+
