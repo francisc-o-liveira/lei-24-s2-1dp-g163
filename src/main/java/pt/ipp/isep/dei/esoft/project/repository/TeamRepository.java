2ef2bb552229ed4972c1acc55a4847fccf4fe813
Francisco <francisc.o.liveira@outlook.com>
2024-04-17 21:02:07 +0100

    Team Repository Start Code

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/TeamRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/TeamRepository.java
@@ -5,17 +5,35 @@ import pt.ipp.isep.dei.esoft.project.domain.Team;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class TeamRepository {
 
     private List<Team> teams;
 
-    public TeamRepository(Team team){
-        teams.add(team);
+    public TeamRepository(){
+        teams = new ArrayList<>();
     }
-    /*public List<Team> getTeam(){
 
-    }*/
+    public Optional<Team> addTeam(Team team){
+        Optional<Team> newTeam = Optional.empty();
+        boolean operationSucess = false;
+        if(teamIsValid(team)){
+            newTeam = Optional.of(team.clone());
+            operationSucess = teams.add(newTeam.get());
+        }
+        if (!operationSucess){
+            newTeam = Optional.empty();
+        }
+        return newTeam;
+    }
+
+    public List<Team> removeTeam(Team team){
+        if (teams.contains(team)){
+            teams.remove(team);
+        }
+        return teams;
+    }
 
     public List<Team> getTeamBySkill(Skill skills){
         List<Team> teamWithSkills=new ArrayList<>();
