d04c6aa4a36443ef3662771604d0388e2929ade0
susanaml27 <1220804@isep.ipp.pt>
2024-04-18 18:42:50 +0100

    coding Controllers

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterJobCategoryController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterJobCategoryController.java
@@ -1,16 +1,24 @@
 package pt.ipp.isep.dei.esoft.project.application.controller;
 
+import pt.ipp.isep.dei.esoft.project.domain.JobCategory;
+import pt.ipp.isep.dei.esoft.project.repository.JobCategoryRepository;
+
+import java.util.List;
+
 public class RegisterJobCategoryController {
 
-    public getJobCategoryRepository(){
+    public JobCategoryRepository jobCategoryRepository;
 
+    public JobCategoryRepository getJobCategoryRepository(){
+        return jobCategoryRepository;
     }
 
     public void registerJobCategory(String job){
-
+        JobCategory jobCategory=new JobCategory(job);
+        jobCategoryRepository.add(jobCategory);
     }
 
-    public getJobCategoriesList(){
-
+    public List<JobCategory> getJobCategoriesList(){
+        return jobCategoryRepository.getJobCategoryList();
     }
 }
