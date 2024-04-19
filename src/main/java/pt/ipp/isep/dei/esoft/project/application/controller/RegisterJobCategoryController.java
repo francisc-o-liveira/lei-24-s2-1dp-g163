bbcbb1c9dce3692b58ab1a1eda2f1dc862b3adcf
susanaml27 <1220804@isep.ipp.pt>
2024-04-19 14:01:12 +0100

    finished RegisterJobCategoryController

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterJobCategoryController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterJobCategoryController.java
@@ -14,8 +14,21 @@ public class RegisterJobCategoryController {
     }
 
     public void registerJobCategory(String job){
+        verifyJobName(job);
         JobCategory jobCategory=new JobCategory(job);
         jobCategoryRepository.add(jobCategory);
+    }
+
+    private void verifyJobName(String job){
+        if(job.trim().isEmpty()){
+            throw new NullPointerException("The Job Name is empty. Please introduce a valid Job Name.");
+        }
+        char[] verification= job.trim().toCharArray();
+        for(char character : verification){
+            if(!Character.isLetter(character)){
+                throw new IllegalArgumentException("The Job Name can't contain any special characters or digits. Please introduce a valid Job Name.");
+            }
+        }
     }
 
     public List<JobCategory> getJobCategoriesList(){
