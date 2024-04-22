f55cc454205f5bfdc14a30626fab685d2017d98f
Francisco <francisc.o.liveira@outlook.com>
2024-04-22 20:29:27 +0100

    testUI

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module pt.ipp.isep.dei.esoft.project.ui.gui {
     requires javafx.controls;
     requires javafx.fxml;
     requires AuthLib;
+    requires org.apache.commons.lang3;
+    requires java.logging;
 
 
     opens pt.ipp.isep.dei.esoft.project.ui.gui to javafx.fxml;
