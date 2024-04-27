4d06836d743efb501e0bf6cbcaef585800062775
susanaml27 <1220804@isep.ipp.pt>
2024-04-27 15:11:18 +0100

    starting UI

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/ManageCollaboratorsUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/ManageCollaboratorsUI.java
@@ -1,6 +1,9 @@
 package pt.ipp.isep.dei.esoft.project.ui.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DatePicker;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -8,15 +11,66 @@ import java.awt.*;
 public class ManageCollaboratorsUI {
 
     public Stage stage = LoginUI.getMainStage();
+    @FXML
+    private TextField addressCity;
+
+    @FXML
+    private TextField addressStreet;
+
+    @FXML
+    private TextField addressZipCode;
+
+    @FXML
+    private DatePicker dateAdmission;
+
+    @FXML
+    private DatePicker dateBirthday;
+
+    @FXML
+    private TextField docIDNumber;
+
+    @FXML
+    private TextField email;
+
 
     @FXML
     private TextField name;
 
     @FXML
-    private TextField dateBirthday;
+    private TextField phoneNumber;
 
     @FXML
-    private TextField admissionDate;
+    public void btnAddCollaborator(ActionEvent event) {
 
+    }
 
+    @FXML
+    void btnEditCollaborator(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRemoveCollaborator(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateCollaborator(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectDocType(ActionEvent event) {
+
+    }
+
+    private Alert popUp(Alert.AlertType alertType, String header, String message) {
+        Alert alerta = new Alert(alertType);
+
+        alerta.setTitle("ERROR");
+        alerta.setHeaderText(header);
+        alerta.setContentText(message);
+
+        return alerta;
+    }
 }
