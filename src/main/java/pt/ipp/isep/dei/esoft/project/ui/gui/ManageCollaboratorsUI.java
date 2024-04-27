e60df071f102c5033cede8b216b03518cd39dedb
Francisco <francisc.o.liveira@outlook.com>
2024-04-27 02:27:29 +0100

    please implement ui better

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/ManageCollaboratorsUI.java
@@ -0,0 +1,22 @@
+package pt.ipp.isep.dei.esoft.project.ui.gui;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class ManageCollaboratorsUI {
+
+    public Stage stage = LoginUI.getMainStage();
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField dateBirthday;
+
+    @FXML
+    private TextField admissionDate;
+
+
+}
