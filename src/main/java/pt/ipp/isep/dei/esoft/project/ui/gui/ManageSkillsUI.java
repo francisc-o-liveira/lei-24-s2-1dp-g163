8f301bffb2e7c69e079d816c2a814bb7b4399521
susanaml27 <1220804@isep.ipp.pt>
2024-04-28 19:45:54 +0100

    ManageSkillsUI and ManageJobsUI finished

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/ManageSkillsUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/ManageSkillsUI.java
@@ -3,17 +3,21 @@ package pt.ipp.isep.dei.esoft.project.ui.gui;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 import pt.ipp.isep.dei.esoft.project.application.controller.RegisterSkillController;
 import pt.ipp.isep.dei.esoft.project.domain.collaborator.Skill;
 
+import java.io.IOException;
+
 
 public class ManageSkillsUI {
-
+    public Stage stage = LoginUI.getMainStage();
     @FXML
     public TextField introducingSkill;
 
@@ -101,5 +105,32 @@ public class ManageSkillsUI {
         alerta.setContentText(message);
 
         return alerta;
+    }
+
+    @FXML
+    public void doLogout(ActionEvent event) throws IOException {
+        Alert popUp= new Alert(Alert.AlertType.CONFIRMATION);
+
+        popUp.setHeaderText("Logging Out");
+        popUp.setContentText("Do you wish to log out?");
+        ((Button) popUp.getDialogPane().lookupButton(ButtonType.OK)).setText("Yes");
+        ((Button) popUp.getDialogPane().lookupButton(ButtonType.CANCEL)).setText("No");
+
+        if (popUp.showAndWait().get() == ButtonType.OK) {
+            FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("/fxml/SceneLogin.fxml"));
+            Parent root= fxmlLoader.load();
+            Scene scene= new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+    @FXML
+    public void goBack(ActionEvent event) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/SceneMenu_HRM.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
