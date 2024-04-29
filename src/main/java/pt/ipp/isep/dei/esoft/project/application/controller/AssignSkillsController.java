78e0bc15c5a7e2b66e19fe065f25d652f5bee9dd
susanaml27 <1220804@isep.ipp.pt>
2024-04-29 13:01:26 +0100

    US04 finished

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/AssignSkillsController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/AssignSkillsController.java
@@ -6,8 +6,8 @@ import pt.ipp.isep.dei.esoft.project.repository.CollaboratorRepository;
 import pt.ipp.isep.dei.esoft.project.repository.Repositories;
 import pt.ipp.isep.dei.esoft.project.repository.SkillRepository;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class AssignSkillsController {
 
@@ -16,91 +16,83 @@ public class AssignSkillsController {
     public SkillRepository skillRepository;
 
     public AssignSkillsController(){
-        getCollaboratorRepository();
-        getSkillRepository();
+        getDataNeededToAssign();
     }
 
-    /**Returns the repository of Skills
+    /** Gets the repositories
      *
-     * @return repository of Skills
      */
-    public SkillRepository getSkillRepository(){
+    public void getDataNeededToAssign(){
         if (skillRepository==null) {
             Repositories repositories=Repositories.getInstance();
             skillRepository=repositories.getSkillRepository();
         }
-        return skillRepository;
-    }
-
-    /**Returns the repository of Collaborators
-     *
-     * @return collaborator repository
-     */
-    public CollaboratorRepository getCollaboratorRepository(){
         if(collaboratorRepository==null){
             Repositories repositories=Repositories.getInstance();
             collaboratorRepository=repositories.getCollaboratorRepository();
         }
-        return collaboratorRepository;
     }
 
-
-
-    /**This method assigns Skills to a Collaborator
+    /**Gets the list of collaborators
      *
-     * @param docIDNumber
-     * @param skills
-     * @throws NullPointerException if the collaborator is not found, which means the collaborator does not exist
-     * @throws IllegalArgumentException if the skill does not exist
+     * @return List of Collaborators
      */
-    public void assignSkills(int docIDNumber, List<Skill> skills){
-        Collaborator assigningCollaborator=collaboratorRepository.searchForCollaborator(docIDNumber);
-        if(assigningCollaborator==null){
-            throw new NullPointerException("The Collaborator does not exist.");
-        }
-        for(Skill s : skills){
-            if(skillRepository.verifyIfExistAndSave(s)){
-                throw new IllegalArgumentException("The Skill does not exist.");
-            }
-            /*if(assigningCollaborator.verifyIfHaveSkill(s)){
-                throw new NullPointerException("The Collaborator already has this Skill."); //needs to be changed
-            }*/
-            assigningCollaborator.setAddSkill(s);
-        }
+    public List<Collaborator> getCollaboratorList(){
+        return collaboratorRepository.getCollaboratorList();
     }
 
-    /** This method shows the details of the collaborator
+    /** Searches for Collaborator given the index
      *
-     * @return details of Collaborator
-     * @throws NullPointerException if the collaborator is not found, which means the collaborator does not exist
+     * @param index of Collaborator
+     * @return collaborator with that ID Number
      */
-    public Collaborator showCollaborator(int docIDNumber){
-        Collaborator collaborator= collaboratorRepository.searchForCollaborator(docIDNumber);
-        if(collaborator==null){
-            throw new NullPointerException("The Collaborator does not exist.");
-        }
-        return collaborator;
+    public Collaborator getCollaborator(int index){
+        return collaboratorRepository.searchForCollaborator(index);
     }
 
-
-    /** Method to show Skills that have been assigned to a Collaborator
+    /** Gets the list of Skills of the collaborator
      *
      * @param collaborator
-     * @param allSkills
+     * @return list of skills of Collaborator
      */
-    public List<Skill> filterAssignedSkills(Collaborator collaborator, List<Skill> allSkills){
-        List<Skill> skillsOfCollaborator=new ArrayList<>();
-        for(Skill s : allSkills){
-           if(collaborator.verifyIfHaveSkill(s)){
-               skillsOfCollaborator.add(s);
-           }
-        }
-        return skillsOfCollaborator;
+
+    public List<Skill> getCollaboratorSkills(Collaborator collaborator){
+        return collaboratorRepository.getCollaboratorSkillsList(collaborator);
     }
 
-    /*public boolean checkAuthorization(Employee userEmail){
+    /**Gets the list of all the skills
+     *
+     * @return All Skills
+     */
 
-    }*/
+    public List<Skill> getAllSkills(){
+        return skillRepository.getSkillList();
+    }
 
+    /** Gets the list of Skills that Collaborator does not have
+     *
+     * @param collaborator
+     * @return list of Skills left to assign to Collaborator
+     */
 
+    public List<Skill> filterSkillsToAssign(Collaborator collaborator){
+        List<Skill> collaboratorSkills=getCollaboratorSkills(collaborator);
+        List<Skill> allSkills=getAllSkills();
+        List<Skill> skillsLeftToAssign = null;
+        for(Skill skill : allSkills){
+            if(!collaboratorSkills.contains(skill)){
+                skillsLeftToAssign.add(skill);
+            }
+        }
+        return skillsLeftToAssign;
+    }
+
+    /**This method assigns a Skill to a Collaborator
+     *
+     * @return collaborator if skill has been assigned
+     */
+    public Optional<Collaborator> assignSkills(Collaborator collaborator, Skill skillName){
+        return collaboratorRepository.assignSkill(collaborator,skillName);
+    }
 }
+
