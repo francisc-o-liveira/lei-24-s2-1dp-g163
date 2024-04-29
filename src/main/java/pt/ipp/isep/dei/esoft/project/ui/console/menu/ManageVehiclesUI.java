574c7d1fa92a216217c832a9f827725ee4f86e54
Francisco <francisc.o.liveira@outlook.com>
2024-04-29 17:35:10 +0100

    UI corrections on JobCategory UI and Skill UI

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/ManageVehiclesUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/ManageVehiclesUI.java
@@ -1,12 +1,16 @@
 package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 
+import pt.ipp.isep.dei.esoft.project.application.controller.authorization.AuthenticationController;
 import pt.ipp.isep.dei.esoft.project.ui.console.*;
 import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
+import pt.isep.lei.esoft.auth.mappers.dto.UserRoleDTO;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ManageVehiclesUI implements Runnable{
+    private AuthenticationController ctrl;
     public ManageVehiclesUI(){
     }
     @Override
@@ -15,9 +19,10 @@ public class ManageVehiclesUI implements Runnable{
         options.add(new MenuItem("1 - Show Vehicle List", new ManageVehiclesUI()));
         options.add(new MenuItem("2 - Remove Vehicle", new ShowTextUI("Implementing.......")));
         options.add(new MenuItem("3 - Add Vehicle", new RegisterVehicleUI()));
-        options.add(new MenuItem("4 - Show CheckUp List", new CheckUpListUI()));
+        options.add(new MenuItem("4 - Show Check Up List", new CheckUpListUI()));
         options.add(new MenuItem("5 - Register a CheckUp", new RegisterCheckUpUI()));
         options.add(new MenuItem("6 - Register update Kilometers", new ManageVehiclesUI()));
+        options.add(new MenuItem("7 - Return MainMenu", getUserMainUI()));
         int option = 0;
         do {
             option = Utils.showAndSelectIndex(options, "\n\n--- ADMIN MENU -------------------------");
@@ -26,5 +31,34 @@ public class ManageVehiclesUI implements Runnable{
                 options.get(option).run();
             }
         } while (option != -1);
+    }
+
+    private Runnable getUserMainUI() {
+        ctrl = new AuthenticationController();
+        List<UserRoleDTO> roles = this.ctrl.getUserRoles();
+        if ((roles == null) || (roles.isEmpty())) {
+            System.out.println("No role assigned to user.");
+        } else {
+            UserRoleDTO role = selectsRole(roles);
+            if (!Objects.isNull(role)) {
+                if(role.equals(ctrl.ROLE_HRM)){
+                    return new HRManagerUI();
+                }
+                if (role.equals(ctrl.ROLE_GSM)){
+                    return new AdminUI();
+                }
+            } else {
+                System.out.println("No role selected.");
+            }
+        }
+        return null;
+    }
+
+    private UserRoleDTO selectsRole(List<UserRoleDTO> roles) {
+        if (roles.size() == 1) {
+            return roles.get(0);
+        } else {
+            return (UserRoleDTO) Utils.showAndSelectOne(roles, "Select the role you want to adopt in this session:");
+        }
     }
 }
