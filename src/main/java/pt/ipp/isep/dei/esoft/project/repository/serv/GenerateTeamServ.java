14639e0c27fbea43e99231d9f1092e736823b951
Francisco <francisc.o.liveira@outlook.com>
2024-05-01 13:43:31 +0100

    update tests/ update Team Generate Design US005 completed

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/serv/GenerateTeamServ.java
@@ -0,0 +1,140 @@
+package pt.ipp.isep.dei.esoft.project.repository.serv;
+
+import pt.ipp.isep.dei.esoft.project.domain.collaborator.Collaborator;
+import pt.ipp.isep.dei.esoft.project.domain.collaborator.Skill;
+import pt.ipp.isep.dei.esoft.project.domain.team.Team;
+
+import java.util.List;
+import java.util.Optional;
+
+public class GenerateTeamServ {
+    public Optional<Team> generateTeam(int minSizeTeam, int maxSizeTeam, List<Skill> skillsSelected, List<Integer> numberCollabForSkill, List<Collaborator> collaboratorsForTeam) {
+        Optional<Team> teamGenerated = Optional.empty();
+        getSkillByOrder(skillsSelected,numberCollabForSkill);
+        int[] generatedTrue=getNumberSkill(numberCollabForSkill);
+        Team create = new Team(maxSizeTeam,minSizeTeam,skillsSelected);
+        boolean[][] YesOrNoSkill = getCollabOrderWithMoreSkill(skillsSelected,collaboratorsForTeam);
+        for (int i = 0; i < YesOrNoSkill.length; i++){
+            if(collabororatorUpgradeTeam(YesOrNoSkill[i], generatedTrue)){
+                if(!create.addCollaborator(collaboratorsForTeam.get(i))){
+                    throw new RuntimeException("Impossible to create Team with this size");
+                }
+                for(int h = 0; h < generatedTrue.length; h++){
+                    if (YesOrNoSkill[i][h]){
+                        generatedTrue[h]--;
+                    }
+                }
+            }
+            if(teamIsCompleted(generatedTrue) && create.getSize()>minSizeTeam){
+                break;
+            }
+        }
+        if(teamIsPossible(create)){
+            teamGenerated = Optional.of(create);
+        }
+        return teamGenerated;
+    }
+
+
+    private boolean[][] getCollabOrderWithMoreSkill(List<Skill> skillsSelected, List<Collaborator> collaboratorsForTeam) {
+        boolean[][] yesOrNoSkill = new boolean[collaboratorsForTeam.size()][skillsSelected.size()];
+        int column;
+        int line = -1;
+        for(Collaborator c : collaboratorsForTeam){
+            line++;
+            column=-1;
+            for (Skill s : skillsSelected){
+                column++;
+                if(c.verifyIfHaveSkill(s)){
+                    yesOrNoSkill[line][column]=true;
+                }
+            }
+        }
+        sortCollabsWithMoreSkill(yesOrNoSkill,collaboratorsForTeam);
+        return yesOrNoSkill;
+    }
+
+    private void sortCollabsWithMoreSkill(boolean[][] yesOrNoSkill, List<Collaborator> collaboratorsForTeam) {
+        Collaborator save;
+        for (int i = 0; i < collaboratorsForTeam.size(); i++){
+            for (int j = i + 1; j < collaboratorsForTeam.size(); j++){
+                if(numberSkillCollab(yesOrNoSkill, j) > numberSkillCollab(yesOrNoSkill, i)){
+                    save=collaboratorsForTeam.get(i);
+                    collaboratorsForTeam.remove(i);
+                    collaboratorsForTeam.set(i,collaboratorsForTeam.get(j-1));
+                    collaboratorsForTeam.remove(j);
+                    collaboratorsForTeam.set(j,save);
+                }
+            }
+        }
+    }
+
+    private int numberSkillCollab(boolean[][] yesOrNoSkill, int i) {
+        int count=0;
+        for (int j = 0 ; j < yesOrNoSkill[0].length; j++){
+            if(yesOrNoSkill[i][j]==true){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean collabororatorUpgradeTeam(boolean[] yesOrNoSkill, int[] generatedTrue) {
+        if (generatedTrue.length==0){
+            return true;
+        }
+        for (int i = 0; i < generatedTrue.length; i++){
+            if(generatedTrue[i]>0 && yesOrNoSkill[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean teamIsPossible(Team create) {
+        return create.isPossible();
+    }
+
+    private boolean teamIsCompleted(int[] generatedTrue) {
+        for (int i = 0; i < generatedTrue.length; i++) {
+            if(generatedTrue[i]>0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int[] getNumberSkill(List<Integer> numberCollabForSkill) {
+        int[] numb = new int[numberCollabForSkill.size()];
+        for (int i = 0; i < numberCollabForSkill.size(); i++) {
+            numb[i]=numberCollabForSkill.get(i);
+        }
+        return numb;
+    }
+
+    private void getSkillByOrder(List<Skill> skillsSelected, List<Integer> numberCollabForSkill) {
+        int save;
+        Skill saveSkill;
+        for(int i = 0; i < skillsSelected.size(); i++){
+            for (int j = i +1; j < skillsSelected.size(); j++){
+                if(numberCollabForSkill.get(i) < numberCollabForSkill.get(j)){
+                    saveSkill=skillsSelected.get(i);
+                    save = numberCollabForSkill.get(i);
+                    numberCollabForSkill.remove(i);
+                    skillsSelected.remove(i);
+                    skillsSelected.set(i,skillsSelected.get(j-1));
+                    numberCollabForSkill.set(i,numberCollabForSkill.get(j-1));
+                    numberCollabForSkill.remove(j);
+                    skillsSelected.remove(j);
+                    if(j==numberCollabForSkill.size()){
+                        skillsSelected.add(saveSkill);
+                        numberCollabForSkill.add(save);
+                    }
+                    skillsSelected.set(j,saveSkill);
+                    numberCollabForSkill.set(j,save);
+                }
+            }
+        }
+    }
+
+}
