ca89c3403e34bb553042d1a715cbd8daf43a7bec
Francisco <francisc.o.liveira@outlook.com>
2024-05-03 20:07:13 +0100

    searchForVehicleByPlate

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/ViewDetailsVehicleUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/ViewDetailsVehicleUI.java
@@ -1,4 +1,63 @@
 package pt.ipp.isep.dei.esoft.project.ui.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import pt.ipp.isep.dei.esoft.project.domain.vehicle.Vehicle;
+
+import java.awt.*;
+
 public class ViewDetailsVehicleUI {
+    @FXML
+    private TextField brand;
+
+    @FXML
+    private TextField model;
+
+    @FXML
+    private TextField type;
+
+    @FXML
+    private TextField tare;
+
+    @FXML
+    private TextField grossWeight;
+
+    @FXML
+    private TextField currentKm;
+
+    @FXML
+    private ChoiceBox<Vehicle.Type> type;
+
+    @FXML
+    private TextField tare;
+
+    @FXML
+    private TextField grossWeight;
+
+    @FXML
+    private TextField currentKm;
+
+    @FXML
+    private TextField checkUpFrequency;
+
+    @FXML
+    private DatePicker registerDate;
+
+    @FXML
+    private DatePicker acquisitionDate;
+
+    @FXML
+    private TableColumn<Vehicle, String> checkUpKilometers;
+
+    @FXML
+    private TableColumn<Vehicle, String> dateOfCheck;
+
+
+
+
+
+    public void putInTextFields(Vehicle selectedCollaborator) {
+    }
 }
