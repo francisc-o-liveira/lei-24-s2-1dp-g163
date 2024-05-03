ecc12ad158dc2bfd50eb04af17c937b9a730d49d
Francisco <francisc.o.liveira@outlook.com>
2024-05-03 10:31:49 +0100

    doLogout implementing Complete JavaFX

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/ManageSkillsUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/ManageSkillsUI.java
@@ -127,6 +127,7 @@ public class ManageSkillsUI {
         ((Button) popUp.getDialogPane().lookupButton(ButtonType.CANCEL)).setText("No");
 
         if (popUp.showAndWait().get() == ButtonType.OK) {
+            ctrlAuth.doLogout();
             FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("/fxml/SceneLogin.fxml"));
             Parent root= fxmlLoader.load();
             Scene scene= new Scene(root);
