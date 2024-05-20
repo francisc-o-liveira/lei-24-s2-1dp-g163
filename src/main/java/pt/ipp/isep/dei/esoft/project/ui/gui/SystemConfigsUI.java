847d34deb55adc3f019d8990fdddf6807167443f
susanaml27 <1220804@isep.ipp.pt>
2024-05-20 15:17:07 +0100

    added SystemConfigs

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/SystemConfigsUI.java
@@ -0,0 +1,62 @@
+package pt.ipp.isep.dei.esoft.project.ui.gui;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import pt.ipp.isep.dei.esoft.project.application.controller.authorization.AuthenticationController;
+
+import java.util.ArrayList;
+
+public class SystemConfigsUI {
+
+    AuthenticationController ctrl=new AuthenticationController();
+    public Stage stage;
+    @FXML
+    private TextField emailTxt;
+
+    @FXML
+    private TextField nameTxt;
+
+    @FXML
+    private TextField passwordTxt;
+
+    @FXML
+    private ComboBox<String> roles;
+    public void setComboBoxAndStage(Stage stage){
+        ArrayList<String> rolesArray=new ArrayList<>();
+        rolesArray.add("HRM");
+        rolesArray.add("VFM");
+        rolesArray.add("GSM");
+        ObservableList<String> rolesForBox= FXCollections.observableArrayList(rolesArray);
+        roles.setItems(rolesForBox);
+        this.stage=stage;
+    }
+    @FXML
+    void addManager(ActionEvent event) {
+        String name=nameTxt.getText();
+        String email=emailTxt.getText();
+        String password=passwordTxt.getText();
+        String role=roles.getValue();
+
+        ctrl.addUserWithRole(name,email,password,role);
+        if(popUp().showAndWait().get()== ButtonType.OK){
+            stage.close();
+        }
+    }
+
+    private Alert popUp(){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("Information");
+        alert.setContentText("Manager added!");
+
+        return alert;
+    }
+
+
+}
