75b3a2811dea41e0a57310ae103deb0ebabe1932
susanaml27 <1220804@isep.ipp.pt>
2024-05-23 09:56:15 +0100

    alterations on SystemConfigs + starting UI for US21

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/main/SystemConfigsUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/main/SystemConfigsUI.java
@@ -2,14 +2,25 @@ package pt.ipp.isep.dei.esoft.project.ui.gui.main;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
+import javafx.collections.SetChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import javafx.scene.control.TextField;
+import pt.ipp.isep.dei.esoft.project.application.controller.OrganizationController;
 import pt.ipp.isep.dei.esoft.project.application.controller.authorization.AuthenticationController;
+import pt.ipp.isep.dei.esoft.project.domain.employee.Manager;
+import pt.ipp.isep.dei.esoft.project.repository.AuthenticationRepository;
+import pt.isep.lei.esoft.auth.AuthFacade;
+import pt.isep.lei.esoft.auth.domain.model.Email;
+import pt.isep.lei.esoft.auth.domain.model.User;
+import pt.isep.lei.esoft.auth.domain.model.UserRole;
+import pt.isep.lei.esoft.auth.domain.store.UserStore;
+
+import java.util.Collections;
+
 
 public class SystemConfigsUI {
 
@@ -26,10 +37,39 @@ public class SystemConfigsUI {
 
     @FXML
     private ComboBox<String> roles;
+
+    @FXML
+    private TableColumn<Manager, String> colEmail;
+
+    @FXML
+    private TableColumn<Manager, String> colManagers;
+
+    @FXML
+    private TableColumn<Manager, String> colType;
+    @FXML
+    private TableView<Manager> tableSystemConfigs;
+
+    private OrganizationController org=new OrganizationController();
+    private ObservableList<Manager> managers=FXCollections.observableSet(org.getList());
+
+
     public void setComboBoxAndStage(Stage stage){
         ObservableList<String> rolesForBox= FXCollections.observableArrayList(ctrl.getRolesToSelect());
         roles.setItems(rolesForBox);
         this.stage=stage;
+    }
+
+    public void setTableSystemConfigs(){
+        colManagers.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colType.setCellValueFactory(new PropertyValueFactory<>("position"));
+        tableSystemConfigs.setItems(managers);
+    }
+
+
+    @FXML
+    void btnRemove(ActionEvent event) {
+
     }
     @FXML
     void addManager(ActionEvent event) {
