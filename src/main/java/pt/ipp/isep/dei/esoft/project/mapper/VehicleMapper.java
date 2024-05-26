5350113348280a0c0ce016b010b5985e8f776346
Francisco <francisc.o.liveira@outlook.com>
2024-05-26 15:43:42 +0100

    Code and Implementing (TimePeriod and Comparators for Entry)

--- src/main/java/pt/ipp/isep/dei/esoft/project/mapper/VehicleMapper.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/mapper/VehicleMapper.java
@@ -1,4 +1,21 @@
 package pt.ipp.isep.dei.esoft.project.mapper;
 
+import pt.ipp.isep.dei.esoft.project.domain.dto.VehicleDto;
+import pt.ipp.isep.dei.esoft.project.domain.vehicle.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class VehicleMapper {
+    public List<VehicleDto> vehicleListToVehicleDtoList(List<Vehicle> vehicles) {
+        List<VehicleDto> vehicleDtos = new ArrayList<>();
+        for (Vehicle vehicle : vehicles) {
+            vehicleDtos.add(vehicleToVehicleDto(vehicle));
+        }
+        return vehicleDtos;
+    }
+
+    public VehicleDto vehicleToVehicleDto(Vehicle vehicle) {
+        return new VehicleDto();
+    }
 }
