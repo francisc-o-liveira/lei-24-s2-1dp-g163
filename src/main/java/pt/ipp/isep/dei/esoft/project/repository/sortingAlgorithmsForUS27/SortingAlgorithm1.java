67d307001ece53fc132eac91a8448170da277bb6
susanaml27 <1220804@isep.ipp.pt>
2024-06-02 03:20:24 +0100

    US27 - sorting algorithms

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/sortingAlgorithmsForUS27/SortingAlgorithm1.java
@@ -0,0 +1,25 @@
+package pt.ipp.isep.dei.esoft.project.repository.sortingAlgorithmsForUS27;
+
+import pt.ipp.isep.dei.esoft.project.domain.dto.GreenSpaceDto;
+
+import java.util.List;
+
+/**Sorting algorithm for the green spaces*/
+public class SortingAlgorithm1 implements SortingList{
+
+    @Override
+    public List<GreenSpaceDto> sortingList(List<GreenSpaceDto> list) {
+        for(int i=0;i< list.size()-1;i++){
+            int biggestIndex=i;
+            for(int j=i+1; j<list.size(); j++){
+                if(list.get(j).getAreaInHectares()>list.get(biggestIndex).getAreaInHectares()){
+                    biggestIndex=j;
+                }
+            }
+            GreenSpaceDto temp=list.get(biggestIndex);
+            list.set(biggestIndex,list.get(i));
+            list.set(i,temp);
+        }
+        return list;
+    }
+}
