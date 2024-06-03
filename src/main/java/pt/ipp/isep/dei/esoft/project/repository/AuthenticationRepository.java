1976d277c7e600bd3e2e42003e6b1f922bd54c6f
Francisco <francisc.o.liveira@outlook.com>
2024-06-03 12:59:06 +0100

    Utilities Address.java!  Serializable in AuthenticationRepository (Different case, because are an example of DataBase)

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/AuthenticationRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/AuthenticationRepository.java
@@ -1,8 +1,16 @@
 package pt.ipp.isep.dei.esoft.project.repository;
 
+import pt.ipp.isep.dei.esoft.project.application.controller.authorization.AuthenticationController;
+import pt.ipp.isep.dei.esoft.project.application.controller.authorization.RegisterController;
 import pt.ipp.isep.dei.esoft.project.domain.adapters.SendEmailExternalAPI;
+import pt.ipp.isep.dei.esoft.project.ui.gui.MainApp;
 import pt.isep.lei.esoft.auth.AuthFacade;
 import pt.isep.lei.esoft.auth.UserSession;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class AuthenticationRepository {
 
@@ -31,6 +39,7 @@ public class AuthenticationRepository {
     }
 
     public boolean addUserWithRole(String name, String email, String pwd, String roleId) {
+        storeUserCredentialInDataBase(name, email, pwd, roleId);
         return authenticationFacade.addUserWithRole(name, email, pwd, roleId);
     }
 
@@ -40,6 +49,63 @@ public class AuthenticationRepository {
 
     public static void setSendEmailExternalAPI(SendEmailExternalAPI sendEmailExternalAPI) {
         AuthenticationRepository.sendEmailExternalAPI = sendEmailExternalAPI;
+    }
+
+
+
+    public void storeUserCredentialInDataBase(String name, String email,String pwd,String roleId){
+
+        Boolean emailAlreadyResgistered = false;
+        try {
+            Scanner scanner = new Scanner(MainApp.getAuthDataBaseFile());
+            while (scanner.hasNextLine()){
+
+                if (scanner.nextLine().split(";")[1].equals(email)){
+                    emailAlreadyResgistered = true;
+                    break;
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        if (!emailAlreadyResgistered){
+            try {
+                FileWriter send = new FileWriter(MainApp.getAuthDataBaseFile(), true);
+                send.write(name+";"+email+";"+pwd+";"+roleId);
+                send.write(System.lineSeparator());
+                send.flush();
+                send.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void loadFromAuthDataBase(){
+
+        addUserRole(AuthenticationController.ROLE_ADMIN, AuthenticationController.ROLE_ADMIN);
+        addUserRole(RegisterController.ROLE_HRM, RegisterController.ROLE_HRM);
+        addUserRole(RegisterController.ROLE_GSM, RegisterController.ROLE_GSM);
+        addUserRole(RegisterController.ROLE_VFM, RegisterController.ROLE_VFM);
+        addUserRole(RegisterController.ROLE_COLAB, RegisterController.ROLE_COLAB);
+
+
+        try {
+            Scanner scanner = new Scanner(MainApp.getAuthDataBaseFile());
+            while (scanner.hasNextLine()){
+
+                String currentLine = scanner.nextLine();
+                String[] curentLineSplited = currentLine.split(";");
+                addUserWithRole(curentLineSplited[0],curentLineSplited[1],curentLineSplited[2],curentLineSplited[3]);
+
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/utilities/Address.java
@@ -0,0 +1,122 @@
+package pt.ipp.isep.dei.esoft.project.utilities;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Class Address, the address contains two attributes, which are zipCode and Street
+ */
+public class Address implements Serializable {
+
+    private String zipCode;
+    private String street;
+    private String city;
+
+    /**
+     * Address constructor
+     * <p>
+     * @param zipCode  Zip code of the object
+     * @param street Street of the object
+     */
+    public Address(String zipCode, String street,String city) throws IOException {
+        setStreet(street);
+        setZipCode(zipCode);
+        setCity(city);
+    }
+
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * Resposible for get and return the zipCode
+     * <p>
+     * @return The zipCode
+     */
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    /**
+     * Responsible for set a new zipCode
+     * <p>
+     * @param zipCode The new zipCode
+     */
+    public void setZipCode(String zipCode) throws IOException {
+        if(zipCode.matches("\\d{5}") && zipCode.split("-").length == 2){
+        this.zipCode = zipCode;
+        }else{
+            throw new IllegalArgumentException("WRONG ZIPCODE");
+        }
+
+    }
+    /**
+     * Resposible for get and return the street
+     * <p>
+     * @return The street
+     */
+    public String getStreet() {
+        return street;
+    }
+    /**
+     * Responsible for set a new street
+     * <p>
+     * @param street The new street
+     */
+    public void setStreet(String street) {
+        if (verifyIsOnlyCharacter(street)){
+            this.street = street;
+        }else {
+            throw new IllegalArgumentException("WRONG STREET");
+        }
+    }
+
+
+
+    /** Method to verify if the name only contains characters
+     *
+     * @param name of Collaborator
+     * @return true if the name only contains characters
+     */
+    private boolean verifyIsOnlyCharacter(String name) {
+        char[] chars = name.replaceAll("\\s", "").toCharArray();
+        for (char c : chars){
+            if (!Character.isLetter(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Method responsible for compare two Address objects
+     * <p>
+     * @param o the other object Address
+     * <p>
+     * @return the boolean representing the comaparation
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Address)) return false;
+        Address address = (Address) o;
+        return zipCode.equals(address.zipCode) && street.equals(address.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(zipCode, street,city);
+    }
+
+    @Override
+    public String toString() {
+        return  "\n\t\t|->zipCode: " + zipCode +
+                "\n\t\t|->street: " + street;
+    }
+}
