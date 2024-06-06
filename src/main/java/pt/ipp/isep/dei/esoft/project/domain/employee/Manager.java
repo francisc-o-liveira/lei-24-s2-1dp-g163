b137ed7ea62edbedc54e00b8c4cb0c9e2db1c968
Francisco <francisc.o.liveira@outlook.com>
2024-06-06 20:07:06 +0100

    Last Updates

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/employee/Manager.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/employee/Manager.java
@@ -2,79 +2,155 @@ package pt.ipp.isep.dei.esoft.project.domain.employee;
 
 import java.io.Serializable;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * Represents a Manager in the system.
+ * Implements Serializable to support object serialization.
+ */
 public class Manager implements Serializable {
 
     private String email;
     private String name;
     private String position;
-    public static enum Role{HRM,GSM,VFM}
+    public static enum Role { HRM, GSM, VFM }
     private String phone;
 
-
-    public static Role[] getEnumManagerRoles(){
+    /**
+     * Retrieves all possible roles for a manager.
+     *
+     * @return An array of Role enums.
+     */
+    public static Role[] getEnumManagerRoles() {
         return Role.values();
     }
+
+    /**
+     * Constructs a Manager instance with the specified name, position, phone, and email.
+     *
+     * @param name     the name of the manager
+     * @param position the position of the manager
+     * @param phone    the phone number of the manager
+     * @param email    the email of the manager
+     * @throws IllegalArgumentException if the name, phone, or email are invalid
+     */
     public Manager(String name, String position, String phone, String email) {
         setName(name);
         setPosition(position);
         setPhone(phone);
         setEmail(email);
-
-
     }
 
+    /**
+     * Sets the name of the manager.
+     *
+     * @param name the name to set
+     * @throws IllegalArgumentException if the name contains more than 6 words
+     */
     public void setName(String name) {
-        if (name.split(" ").length<=6){
-            this.name=name;
-        }else {
-            throw new IllegalArgumentException("Name must contain max of 6 characters");
+        if (name.split(" ").length <= 6) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Name must contain a maximum of 6 words");
         }
     }
 
+    /**
+     * Sets the phone number of the manager.
+     *
+     * @param phone the phone number to set
+     * @throws IllegalArgumentException if the phone number is invalid
+     */
     public void setPhone(String phone) {
-        //if (verifyPhone(phone)){
-            this.phone=phone;
-        /*}else {
-            throw new IllegalArgumentException("Phone number is invalid please try again");
-        }*/
+        if (verifyPhone(phone)) {
+            this.phone = phone;
+        } else {
+            throw new IllegalArgumentException("Phone number is invalid. Please try again.");
+        }
     }
 
+    /**
+     * Verifies if the given phone number is valid.
+     *
+     * @param phone the phone number to verify
+     * @return true if the phone number is valid, false otherwise
+     */
     private boolean verifyPhone(String phone) {
-        return phone.matches("^\\+(?:[0-9] ?){6,14}[0-9]$");
+        String verify = "^\\+(?:[0-9] ?){6,14}[0-9]$";
+        Pattern pattern = Pattern.compile(verify);
+        Matcher matcher = pattern.matcher(phone);
+        return matcher.matches();
     }
 
+    /**
+     * Sets the position of the manager.
+     *
+     * @param position the position to set
+     */
     public void setPosition(String position) {
         this.position = position;
     }
+
+    /**
+     * Sets the email of the manager.
+     *
+     * @param email the email to set
+     * @throws IllegalArgumentException if the email is invalid
+     */
     public void setEmail(String email) {
-        if (verifyCollaboratorEmail(email)){
-            this.email=email;
-        }else {
+        if (verifyCollaboratorEmail(email)) {
+            this.email = email;
+        } else {
             throw new IllegalArgumentException("Invalid email");
         }
-
     }
 
+    /**
+     * Verifies if the given email is valid.
+     *
+     * @param email the email to verify
+     * @return true if the email is valid, false otherwise
+     */
     private boolean verifyCollaboratorEmail(String email) {
         String[] emailDiv = email.split("@");
-        if (emailDiv[1].split("\\.").length ==2 && email.split("@").length==2){
-            return true;
-        }
-        return false;
+        return emailDiv.length == 2 && emailDiv[1].split("\\.").length == 2;
     }
 
+    /**
+     * Gets the email of the manager.
+     *
+     * @return the email of the manager
+     */
     public String getEmail() {
         return email;
     }
+
+    /**
+     * Gets the name of the manager.
+     *
+     * @return the name of the manager
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the position of the manager.
+     *
+     * @return the position of the manager
+     */
     public String getPosition() {
         return position;
     }
 
+    /**
+     * Checks if this manager is equal to another object.
+     * Managers are considered equal if they have the same email.
+     *
+     * @param o the object to compare
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -87,26 +163,43 @@ public class Manager implements Serializable {
         return email.equals(manager.email);
     }
 
+    /**
+     * Computes the hash code for this manager.
+     *
+     * @return the hash code of this manager
+     */
     @Override
     public int hashCode() {
         return Objects.hash(email);
     }
 
+    /**
+     * Checks if this manager has the given email.
+     *
+     * @param email the email to check
+     * @return true if this manager has the given email, false otherwise
+     */
     public boolean hasEmail(String email) {
         return this.email.equals(email);
     }
 
-
     /**
-     * Clone method.
+     * Creates and returns a clone of this manager.
      *
-     * @return A clone of the current instance.
+     * @return a clone of this manager
      */
+    @Override
     public Manager clone() {
-        return new Manager(this.name,this.position,this.phone,this.email);
+        return new Manager(this.name, this.position, this.phone, this.email);
     }
 
-    public String toString(){
-        return String.format("%s %s %s %s", name, email, position,phone);
+    /**
+     * Returns a string representation of the manager.
+     *
+     * @return a string representation of the manager
+     */
+    @Override
+    public String toString() {
+        return String.format("%s %s %s %s", name, email, position, phone);
     }
 }
--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/org/GreenSpace.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/org/GreenSpace.java
@@ -6,10 +6,16 @@ import pt.ipp.isep.dei.esoft.project.utilities.Address;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * Represents a GreenSpace in the system.
+ * Implements Serializable to support object serialization.
+ */
 public class GreenSpace implements Serializable {
 
-
-    public enum Type implements Serializable{MediumSize,LargeSize,Garden}
+    /**
+     * Enum representing the type of GreenSpace.
+     */
+    public enum Type implements Serializable { MediumSize, LargeSize, Garden }
 
     private double areaInHectares;
     private Address address;
@@ -17,8 +23,18 @@ public class GreenSpace implements Serializable {
     private Type type;
     private String email;
 
-
-    public GreenSpace(double area,String name,Type type, String managerEmail, Address address, String email) {
+    /**
+     * Constructs a GreenSpace instance with the specified details.
+     *
+     * @param area         the area of the green space in hectares
+     * @param name         the name of the green space
+     * @param type         the type of the green space
+     * @param managerEmail the email of the manager who created this green space
+     * @param address      the address of the green space
+     * @param email        the email of the creator
+     * @throws IllegalArgumentException if the area is less than or equal to 0, or if the address is null
+     */
+    public GreenSpace(double area, String name, Type type, String managerEmail, Address address, String email) {
         setArea(area);
         setAddress(address);
         setName(name);
@@ -26,6 +42,11 @@ public class GreenSpace implements Serializable {
         setEmailOfCreator(managerEmail);
     }
 
+    /**
+     * Constructs a GreenSpace instance from a GreenSpaceDto.
+     *
+     * @param greenSpaceDto the DTO containing the green space details
+     */
     public GreenSpace(GreenSpaceDto greenSpaceDto) {
         setArea(greenSpaceDto.getAreaInHectares());
         setAddress(greenSpaceDto.getAddress());
@@ -34,84 +55,149 @@ public class GreenSpace implements Serializable {
         setEmailOfCreator(greenSpaceDto.createdBy());
     }
 
+    /**
+     * Sets the type of the green space.
+     *
+     * @param type the type to set
+     */
     private void setType(Type type) {
         this.type = type;
     }
 
+    /**
+     * Sets the area of the green space.
+     *
+     * @param area the area to set
+     * @throws IllegalArgumentException if the area is less than or equal to 0
+     */
     private void setArea(double area) {
-        if(area <= 0){
-            throw new IllegalArgumentException("Area cannot be less or equal to 0");
+        if (area <= 0) {
+            throw new IllegalArgumentException("Area cannot be less than or equal to 0");
         }
         this.areaInHectares = area;
     }
 
+    /**
+     * Sets the address of the green space.
+     *
+     * @param address the address to set
+     * @throws IllegalArgumentException if the address is null
+     */
     private void setAddress(Address address) {
         if (address == null) {
             throw new IllegalArgumentException("Address cannot be null");
-        }else {
-            this.address = address;
         }
+        this.address = address;
     }
 
+    /**
+     * Verifies if the given address is valid.
+     *
+     * @param address the address to verify
+     * @return true if the address is valid, false otherwise
+     */
     private boolean verifyStreet(String address) {
-        char[] chars = address.toCharArray();;
-        for(Character c : chars){
-            if (!Character.isLetter(c) && Character.isDigit(c)){
+        char[] chars = address.toCharArray();
+        for (Character c : chars) {
+            if (!Character.isLetter(c) && Character.isDigit(c)) {
                 return false;
             }
         }
         return true;
     }
 
-    /** Method to verify if the name only contains characters
+    /**
+     * Verifies if the given name contains only characters.
      *
-     * @param name of Collaborator
-     * @return true if the name only contains characters
+     * @param name the name to verify
+     * @return true if the name contains only characters, false otherwise
      */
     private boolean verifyIsOnlyCharacter(String name) {
         char[] chars = name.replaceAll("\\s", "").toCharArray();
-        for (char c : chars){
-            if (!Character.isLetter(c)){
+        for (char c : chars) {
+            if (!Character.isLetter(c)) {
                 return false;
             }
         }
         return true;
     }
 
-
+    /**
+     * Sets the name of the green space.
+     *
+     * @param name the name to set
+     * @throws IllegalArgumentException if the name contains numbers
+     */
     private void setName(String name) {
-        if (verifyIsOnlyCharacter(name)){
+        if (verifyIsOnlyCharacter(name)) {
             this.name = name;
         } else {
-            throw new IllegalArgumentException("Name Cannot have have numbers");
+            throw new IllegalArgumentException("Name cannot contain numbers");
         }
     }
 
+    /**
+     * Gets the area of the green space.
+     *
+     * @return the area in hectares
+     */
     public double getArea() {
         return areaInHectares;
     }
 
+    /**
+     * Gets the address of the green space.
+     *
+     * @return the address
+     */
     public Address getAddress() {
         return address;
     }
 
+    /**
+     * Gets the name of the green space.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the type of the green space.
+     *
+     * @return the type
+     */
     public Type getType() {
         return type;
     }
 
+    /**
+     * Returns a string representation of the green space.
+     *
+     * @return a string representation of the green space
+     */
     @Override
     public String toString() {
         return this.name + "----" + this.areaInHectares + "-----" + this.address;
     }
 
-    public static final Type[] getEnumGreenSpaceTypes(){
+    /**
+     * Retrieves all possible types of green spaces.
+     *
+     * @return an array of Type enums
+     */
+    public static Type[] getEnumGreenSpaceTypes() {
         return Type.values();
     }
 
+    /**
+     * Checks if this green space is equal to another object.
+     * Green spaces are considered equal if they have the same name, area, address, and type.
+     *
+     * @param obj the object to compare
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -128,14 +214,23 @@ public class GreenSpace implements Serializable {
                 Objects.equals(this.getType(), gs.getType());
     }
 
+    /**
+     * Gets the email of the creator of this green space.
+     *
+     * @return the email of the creator
+     */
     public String createdBy() {
         return email;
     }
 
+    /**
+     * Sets the email of the creator of this green space.
+     *
+     * @param managerEmail the manager's email to set
+     */
     private void setEmailOfCreator(String managerEmail) {
         if (managerEmail != null) {
             this.email = managerEmail;
         }
     }
-
 }
