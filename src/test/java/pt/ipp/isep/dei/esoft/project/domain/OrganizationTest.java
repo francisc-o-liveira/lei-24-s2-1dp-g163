92dcfbf39450416646c20d1125cd157e0868060d
Francisco <francisc.o.liveira@outlook.com>
2024-06-06 22:18:44 +0100

    Documentation

--- src/test/java/pt/ipp/isep/dei/esoft/project/domain/OrganizationTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/OrganizationTest.java
@@ -1,9 +1,6 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
 import org.junit.jupiter.api.Test;
-import pt.ipp.isep.dei.esoft.project.domain.employee.Employee;
-import pt.ipp.isep.dei.esoft.project.domain.task.Task;
-import pt.ipp.isep.dei.esoft.project.domain.task.TaskCategory;
 import pt.ipp.isep.dei.esoft.project.repository.Organization;
 
 import java.util.Optional;
