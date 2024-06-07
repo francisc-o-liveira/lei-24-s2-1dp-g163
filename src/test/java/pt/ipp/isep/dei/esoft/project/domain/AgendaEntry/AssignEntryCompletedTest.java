b72417bae89047e518bf11586e8b4262d2b602b7
susanaml27 <1220804@isep.ipp.pt>
2024-06-07 23:42:08 +0100

    minor correction

--- src/test/java/pt/ipp/isep/dei/esoft/project/domain/AgendaEntry/AssignEntryCompletedTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/AgendaEntry/AssignEntryCompletedTest.java
@@ -6,11 +6,15 @@ import pt.ipp.isep.dei.esoft.project.domain.collaborator.Collaborator;
 import pt.ipp.isep.dei.esoft.project.domain.collaborator.DocType;
 import pt.ipp.isep.dei.esoft.project.domain.collaborator.JobCategory;
 import pt.ipp.isep.dei.esoft.project.domain.dto.EntryDto;
+import pt.ipp.isep.dei.esoft.project.domain.dto.GreenSpaceDto;
+import pt.ipp.isep.dei.esoft.project.domain.org.GreenSpace;
 import pt.ipp.isep.dei.esoft.project.domain.task.Entry;
 import pt.ipp.isep.dei.esoft.project.domain.task.EntryState;
 import pt.ipp.isep.dei.esoft.project.domain.task.Task;
 import pt.ipp.isep.dei.esoft.project.repository.EntryRepository;
+import pt.ipp.isep.dei.esoft.project.repository.Organization;
 import pt.ipp.isep.dei.esoft.project.repository.Repositories;
+import pt.ipp.isep.dei.esoft.project.utilities.Address;
 import pt.ipp.isep.dei.esoft.project.utilities.Date;
 import pt.ipp.isep.dei.esoft.project.utilities.Tempo;
 
@@ -24,32 +28,49 @@ public class AssignEntryCompletedTest {
     private EntryDto entryDto;
 
     private Date completionDate;
+    private String name;
+    private String address;
+    private String city;
+    private String zipCode;
+    private double area;
+
+    private String email;
+    private GreenSpace.Type type;
 
     @BeforeEach
     void setUp() {
         entryRepository = Repositories.getInstance().getEntryRepository();
-
+        Organization greenSpaceRepository = Repositories.getInstance().getOrganizationRepository();
+        name = "isep";
+        address = "rua sao tome";
+        city = "Porto";
+        zipCode ="4400-123";
+        area = 20.0;
+        type = GreenSpace.Type.Garden;
+        email = "gsm@this.app";
+        GreenSpaceDto gs=new GreenSpaceDto(area, new Address(zipCode,address,city),name,type,email);
+        greenSpaceRepository.registerGreenSpace(gs);
         entryDto = new EntryDto(
-                new Date(2023, 6, 1),
-                new EntryState(),
+                new Date(2024, 6, 1),
+                new EntryState(EntryState.State.Assigned),
                 "Test Task",
                 "Description of Test Task",
                 Task.DegreeUrgency.Medium,
                 new Tempo(2),
-                null,
-                "REF123"
+                gs,
+                "123"
         );
-
-        completionDate = new Date(2024,6,7);
-
+        completionDate=new Date(2024,6,7);
+        entryRepository.registerNewTask(entryDto);
+        entryRepository.assignEntryOnAgenda(entryDto);
     }
 
     @Test
     void completeTaskTest(){
-        Collaborator c = new Collaborator("Joaquim Mendes Manuel Silva Oliveira",new Date(2001,10,29), new Date(2024,04,29),"Rua Das Rosas","4630-131","Marco de Canaveses","+351 916835384","joaquim@gmail.com", DocType.Type.CitizenCard,197232131,new JobCategory("Gardener"));
+        Collaborator c = new Collaborator("Joaquim Mendes Manuel Silva Oliveira",new Date(2001,10,29), new Date(2024,4,29),"Rua Das Rosas","4630-131","Marco de Canaveses","+351 916835384","joaquim@gmail.com", DocType.Type.CitizenCard,197232131,new JobCategory("Gardener"));
         entryDto.completeTask(completionDate,c);
         Optional<Entry> entry = entryRepository.completeTaskCollaborator(entryDto);
-        assertNotNull(entry.get());
+        assertNotNull(entry.isPresent());
         assertTrue(entry.get().isCompleted());
         assertEquals(entry.get().getStatus().getState(),EntryState.State.Done);
 
--- src/test/java/pt/ipp/isep/dei/esoft/project/domain/AgendaEntry/PostponeEntryTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/AgendaEntry/PostponeEntryTest.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pt.ipp.isep.dei.esoft.project.application.controller.AssignEntryOnAgendaController;
 import pt.ipp.isep.dei.esoft.project.domain.dto.EntryDto;
+import pt.ipp.isep.dei.esoft.project.domain.dto.GreenSpaceDto;
 import pt.ipp.isep.dei.esoft.project.domain.dto.TeamDto;
+import pt.ipp.isep.dei.esoft.project.domain.org.GreenSpace;
 import pt.ipp.isep.dei.esoft.project.domain.task.Entry;
 import pt.ipp.isep.dei.esoft.project.domain.task.EntryState;
 import pt.ipp.isep.dei.esoft.project.domain.task.Task;
 import pt.ipp.isep.dei.esoft.project.repository.EntryRepository;
+import pt.ipp.isep.dei.esoft.project.repository.Organization;
 import pt.ipp.isep.dei.esoft.project.repository.Repositories;
+import pt.ipp.isep.dei.esoft.project.utilities.Address;
 import pt.ipp.isep.dei.esoft.project.utilities.Date;
 import pt.ipp.isep.dei.esoft.project.utilities.Tempo;
 
@@ -25,24 +29,42 @@ public class PostponeEntryTest {
     private Date postDate;
 
     private Tempo startTime;
+    private String name;
+    private String address;
+    private String city;
+    private String zipCode;
+    private double area;
 
+    private String email;
+    private GreenSpace.Type type;
 
     @BeforeEach
     void setUp() {
         entryRepository = Repositories.getInstance().getEntryRepository();
-
+        Organization greenSpaceRepository = Repositories.getInstance().getOrganizationRepository();
+        name = "isep";
+        address = "rua sao tome";
+        city = "Porto";
+        zipCode ="4400-123";
+        area = 20.0;
+        type = GreenSpace.Type.Garden;
+        email = "gsm@this.app";
+        GreenSpaceDto gs=new GreenSpaceDto(area, new Address(zipCode,address,city),name,type,email);
+        greenSpaceRepository.registerGreenSpace(gs);
         entryDto = new EntryDto(
                 new Date(2023, 6, 1),
-                new EntryState(),
+                new EntryState(EntryState.State.Assigned),
                 "Test Task",
                 "Description of Test Task",
                 Task.DegreeUrgency.Medium,
                 new Tempo(2),
-                null,
-                "REF123"
+                gs,
+                "123"
         );
         postDate = new Date(2024,7,30);
         startTime =new Tempo(14,0);
+        entryRepository.registerNewTask(entryDto);
+        entryRepository.assignEntryOnAgenda(entryDto);
     }
 
     @Test
