150d440f74dd64b633ff0bbfad4716fcc37885aa
susanaml27 <1220804@isep.ipp.pt>
2024-06-08 17:50:59 +0100

    corrections

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/MainApp.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/MainApp.java
@@ -94,8 +94,9 @@ public class MainApp extends Application {
         String currentDir = System.getProperty("user.dir");
 
         // Construct the path to the team database file
-        String relativePath = "\\target\\classes\\DataBase\\";
-        currentDir.replace("\"","\\");
+        String relativePath = "\\\\target\\\\classes\\\\DataBase\\\\";
+        currentDir = currentDir.replace("\\", "\\\\");
+
 
         // Combine the current directory with the relative path
         String fullPath = currentDir + relativePath + fileName;
